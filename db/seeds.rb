3c6dce8eea917e730456946b5d77112023655c12
German Garcia Gonzalez <ggarciag.business@outlook.com>
2019-10-02 16:39:52 -0400

    styling changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Topic.destroy_all()
 motivation = Topic.find_or_create_by(name: "Motivational Podcasts")
 
 Podcast.destroy_all()
-garyv = Podcast.find_or_create_by(name: "GaryV", image: "https://upload.wikimedia.org/wikipedia/commons/c/cd/Gary_Vaynerchuk_public_domain.jpg", description: "Realistic Motivation", topic_id: motivation.id)
+garyv = Podcast.find_or_create_by(name: "The Gary Vee Audio Experience", image: "https://upload.wikimedia.org/wikipedia/commons/c/cd/Gary_Vaynerchuk_public_domain.jpg", description: "Realistic Motivation", topic_id: motivation.id)
 
 PersonalNote.destroy_all()
 firstNote = PersonalNote.find_or_create_by(episode: "all of them", rating: "10/10", explanation: "His podcast has value", podcast_id: garyv.id)
